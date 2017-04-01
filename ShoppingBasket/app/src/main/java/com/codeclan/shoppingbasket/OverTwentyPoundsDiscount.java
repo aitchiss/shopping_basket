4fe1cd80bcdccd8757dd6da8f70c1fd6a1926ab5
Suzanne <suzanne.aitchison@gmail.com>
2017-04-01 21:03:51 +0100

    tested overtwenty discount returns correct discount

--- ShoppingBasket/app/src/main/java/com/codeclan/shoppingbasket/OverTwentyPoundsDiscount.java
+++ ShoppingBasket/app/src/main/java/com/codeclan/shoppingbasket/OverTwentyPoundsDiscount.java
@@ -7,8 +7,20 @@ package com.codeclan.shoppingbasket;
 public class OverTwentyPoundsDiscount implements TotalSpendDiscount {
 
     private String description = "10% off total over £20";
+    private double percentageToDiscount = 10.0;
 
     public String getDescription(){
         return this.description;
+    }
+
+    public int calculateDiscounts(Customer customer, int totalSpend){
+        if (totalSpend > 20000){
+            double discount = (this.percentageToDiscount/100) * totalSpend;
+            int totalDiscount = (int) Math.round(discount);
+            return totalDiscount;
+        } else {
+            return 0;
+        }
+
     }
 }
--- ShoppingBasket/app/src/test/java/com/codeclan/shoppingbasket/OverTwentyPoundsDiscountTest.java
+++ ShoppingBasket/app/src/test/java/com/codeclan/shoppingbasket/OverTwentyPoundsDiscountTest.java
@@ -13,15 +13,34 @@ import static org.junit.Assert.assertEquals;
 public class OverTwentyPoundsDiscountTest {
 
     OverTwentyPoundsDiscount overTwentyPoundsDiscount;
+    ShoppingBasket shoppingBasket;
+    Customer customer;
 
     @Before
     public void before(){
         overTwentyPoundsDiscount = new OverTwentyPoundsDiscount();
+        shoppingBasket = new ShoppingBasket();
+//        Item bread = new Item(1234, "hovis bread", 120);
+//        Item lobster = new Item(5632, "lobster dinner for two", 2550);
+//        shoppingBasket.addItem(bread);
+//        shoppingBasket.addItem(lobster);
+
+        customer = new Customer("Suzanne", shoppingBasket);
 
     }
 
     @Test
     public void testDiscountHasDescription(){
         assertEquals("10% off total over £20", overTwentyPoundsDiscount.getDescription());
+    }
+
+    @Test
+    public void testDeductsTenPercentIfTotalOverTwenty(){
+        assertEquals(267, overTwentyPoundsDiscount.calculateDiscounts(customer, 2670));
+    }
+
+    @Test
+    public void testDiscountDoesntApplyUnderTwenty(){
+        assertEquals(0, overTwentyPoundsDiscount.calculateDiscounts(customer, 1000));
     }
 }
